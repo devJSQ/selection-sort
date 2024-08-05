b7b3451ee24b28e526d460ee1e92d7b9dba84522
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-08-05 19:42:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Arrays;
 public class Main {
+  public static int [] SelectionSort(int [] numbers){
+    int n = numbers.length;
+    for (int i = 0; i < n - 1; i++){
+      int val = i;
 
- static void main(String[] args){
+      for (int j = i + 1; j < n; j++){
+        if(numbers[j] < numbers[val]){
+          val = j;
+        }
+      }
+      int temp = numbers[val];
+      numbers[val] = numbers[i];
+      numbers[i] = temp;
+      
+    }
+    return numbers;
 
-          /* your code here */
+
+  }
+
+ public static void main(String[] args){
+  int [] numbers = {90,70,10,1,0,30};
+  System.out.println(Arrays.toString(numbers));
+  SelectionSort(numbers);
+  System.out.println(Arrays.toString(numbers));
   }
 }
+
